f4a045cea97a7097a6600ef318c67a4f7e09496d
Lizzy <>
2023-04-21 18:27:48 +0000

    = invalid syntax

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -124,7 +124,7 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
-=
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CSRF_TRUSTED_ORIGINS = ['https://*.cloudshell.dev'] 
 CSRF_TRUSTED_ORIGINS = [ "https://*.run.app" ]
